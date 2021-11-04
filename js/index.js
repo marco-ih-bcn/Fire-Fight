28cda46b73e9e823152165e6f402e0910be49fb0
Marco Santonastasi <84954908+marco-ih-bcn@users.noreply.github.com>
2021-11-04 11:01:05 +0100

    Update index.js

--- js/index.js
+++ js/index.js
@@ -6,15 +6,15 @@ const buildDom = (html) => {
 
 const buildStartScreen = () => {
     buildDom(`
-        <img src="/img/welcome_text.png" alt="" class="welcome_text">
-        <img src="/img/cloud.png" alt="" class="cloud">
-        <img src="/img/cloud.png" alt="" class="cloud_2">
-        <img src="/img/cloud.png" alt="" class="cloud_3">
-        <img src="/img/fire_extinguisher.png" alt="" id="start_button" class="fire_extinguisher">
-        <img src="/img/arrow.png" alt="" class="arrow">
-        <img src="/img/start.png" alt="" class="start">
+        <img src="./img/welcome_text.png" alt="" class="welcome_text">
+        <img src="./img/cloud.png" alt="" class="cloud">
+        <img src="./img/cloud.png" alt="" class="cloud_2">
+        <img src="./img/cloud.png" alt="" class="cloud_3">
+        <img src="./img/fire_extinguisher.png" alt="" id="start_button" class="fire_extinguisher">
+        <img src="./img/arrow.png" alt="" class="arrow">
+        <img src="./img/start.png" alt="" class="start">
         <div class="instructions_container">
-        <img src="/img/instructions.png" alt="" class="instructions">
+        <img src="./img/instructions.png" alt="" class="instructions">
         </div>
     `)
     const startButton = document.getElementById("start_button");
